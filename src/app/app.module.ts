2f044e01d27e740133ff72c3c56e92e9c776de7b
Angie Nicole <angieandrade9601@gmail.com>
2023-02-18 13:27:30 -0500

    nuevo
    se agrego el modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
@@ -14,6 +15,7 @@ import { RoutesModule } from './routes/routes.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     RouterModule,
     LayoutModule,
     RoutesModule, 
